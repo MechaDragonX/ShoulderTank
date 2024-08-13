29b8c8db225489675cbd0c4b2fadd015dc285c4b
rags <MechaDragonX>
2024-08-12 23:12:14 -0700

    Add forward, backward movement

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -28,35 +28,35 @@ var y_min: float = -30
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var right_x: float = 0.0
-	var right_y: float = 0.0
+    var right_x: float = 0.0
+    var right_y: float = 0.0
 
-	if Input.get_connected_joypads().size() > 0:
-		right_x = Input.get_joy_axis(0, 2)
-		right_y = Input.get_joy_axis(0, 3)
+    if Input.get_connected_joypads().size() > 0:
+        right_x = Input.get_joy_axis(0, 2)
+        right_y = Input.get_joy_axis(0, 3)
 
-		x += -right_x * x_sensitivity_con
-		y += -right_y * y_sensitivity_con
+        x += -right_x * x_sensitivity_con
+        y += -right_y * y_sensitivity_con
 
 func _input(event):
-	if event is InputEventMouseMotion:
-		x += -event.relative.x * x_sensitivity_mouse
-		y += -event.relative.y * y_sensitivity_mouse
-	elif Input.is_action_pressed("reset_camera"):
-		x = 0
-		y = 0
+    if event is InputEventMouseMotion:
+        x += -event.relative.x * x_sensitivity_mouse
+        y += -event.relative.y * y_sensitivity_mouse
+    elif Input.is_action_pressed("reset_camera"):
+        x = 0
+        y = 0
 
 func _physics_process(delta):
-	x = clamp(x, x_min, x_max)
-	y = clamp(y, y_min, y_max)
+    x = clamp(x, x_min, x_max)
+    y = clamp(y, y_min, y_max)
 
-	# x_node.rotation_degrees.y = lerp(x_node.rotation_degrees.y, x, x_acceleration * delta)
-	# y_node.rotation_degrees.x = lerp(y_node.rotation_degrees.x, y, x_accerlation * delta)
-	
-	x_node.rotation_degrees.y = x
-	y_node.rotation_degrees.x = y
+    # x_node.rotation_degrees.y = lerp(x_node.rotation_degrees.y, x, x_acceleration * delta)
+    # y_node.rotation_degrees.x = lerp(y_node.rotation_degrees.x, y, x_accerlation * delta)
+    
+    x_node.rotation_degrees.y = x
+    y_node.rotation_degrees.x = y
---
+++ scripts/movement_controller.gd
@@ -0,0 +1,35 @@
+extends Node
+
+@export var player: CharacterBody3D
+@export var mesh_root: Node3D
+@export var rotation_speed: float = 8
+
+# var direction: Vector3
+var velocity: Vector3
+var acceleration: float
+var speed: float = 8
+
+func _input(event):
+    if Input.is_action_just_released("backward") && (Input.is_action_just_released("sprint") || Input.is_action_just_released("quick_turn_secondary")):
+        #mesh_root.rotate(Vector3(0, 1, 0), 1)
+        pass
+
+func _physics_process(delta):
+    var direction: Vector3 = Vector3.ZERO
+    
+    if Input.is_action_pressed("movement"):
+        if Input.is_action_pressed("forward"):
+            direction.z += 1
+        if Input.is_action_pressed("backward"):
+            direction.z -= 1
+
+    velocity.z = direction.z * speed
+    player.velocity = velocity
+    player.move_and_slide()
+
+#func _on_set_movement_state(_movement_state: MovementState):
+    #speed = _movement_state.movement_speed
+    #acceleration = _movement_state.acceleration
+#
+#func _on_set_movement_direction(_movement_direction: Vector3):
+    #direction = _movement_direction
