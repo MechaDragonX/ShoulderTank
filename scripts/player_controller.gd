0baaa23b1263067a8fe35c8680edc4a495869381
rags <MechaDragonX>
2024-08-13 01:20:14 -0700

    Attach move_con to Player, rename to player_con, and remove now unused MoveCon node

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,6 +1,4 @@
-extends Node
-
-@export var player: CharacterBody3D
+extends CharacterBody3D
 
 # Speed for forward/backward movement and turning left/right
 var move_speed: float = 8
@@ -23,7 +21,7 @@ func _input(event):
     # "just_released" is used because this isn't held like the moving and turning
     if Input.is_action_just_released("backward") && (Input.is_action_just_released("sprint") || Input.is_action_just_released("quick_turn_secondary")):
         # Rotate player 180°
-        player.rotate_y(PI)
+        rotate_y(PI)
 
 func _physics_process(delta):
     # Local rotation variable
@@ -35,10 +33,10 @@ func _physics_process(delta):
         # Base values are modified by the speed,
         # and then modified by the delta to prevent movement being tied to framerate
         if Input.is_action_pressed("forward"):
-            player.translate(Vector3(0, 0, 1 * move_speed) * delta)
+            translate(Vector3(0, 0, 1 * move_speed) * delta)
         # Make sure quick turn inputs aren't pressed to prevent moving during it
         if Input.is_action_pressed("backward") && !(Input.is_action_just_released("sprint") || Input.is_action_just_released("quick_turn_secondary")):
-            player.translate(Vector3(0, 0, -1 * move_speed) * delta)
+            translate(Vector3(0, 0, -1 * move_speed) * delta)
         # Turn left and right 
         if Input.is_action_pressed("turn_left"):
             rotation += 0.1
@@ -46,7 +44,7 @@ func _physics_process(delta):
             rotation -= 0.1
 
     # Rotate player based base value, and turn speed and delta for same reasons as movement
-    player.rotate_y(rotation * turn_speed * delta)
+    rotate_y(rotation * turn_speed * delta)
 
     # Apply movement
-    player.move_and_slide()
+    move_and_slide()
