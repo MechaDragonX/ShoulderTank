418a837d0b98160bc037e10fe38955397d1901e8
rags <MechaDragonX>
2024-08-12 23:29:16 -0700

    Add player turning

--- scripts/movement_controller.gd
+++ scripts/movement_controller.gd
@@ -4,10 +4,12 @@ extends Node
 @export var mesh_root: Node3D
 @export var rotation_speed: float = 8
 
-# var direction: Vector3
 var velocity: Vector3
 var acceleration: float
 var speed: float = 8
+
+func _ready():
+    pass
 
 func _input(event):
     if Input.is_action_just_released("backward") && (Input.is_action_just_released("sprint") || Input.is_action_just_released("quick_turn_secondary")):
@@ -16,15 +18,21 @@ func _input(event):
 
 func _physics_process(delta):
     var direction: Vector3 = Vector3.ZERO
+    var rotation: float = 0
     
     if Input.is_action_pressed("movement"):
         if Input.is_action_pressed("forward"):
             direction.z += 1
         if Input.is_action_pressed("backward"):
             direction.z -= 1
+        if Input.is_action_pressed("turn_left"):
+            rotation += 0.1
+        if Input.is_action_pressed("turn_right"):
+            rotation -= 0.1
 
     velocity.z = direction.z * speed
     player.velocity = velocity
+    player.rotate_y(rotation)
     player.move_and_slide()
 
 #func _on_set_movement_state(_movement_state: MovementState):
